1fcc11045f7404037cb06955513ca8e313881438
saulopereira2018 <37461725+saulopereira2018@users.noreply.github.com>
2021-01-15 08:59:27 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,22 @@
+
+            function doo() {
+                window.navigator.vibrate(261,6256);
+            }
+            function re() {
+                window.navigator.vibrate(293,6648);
+            }
+            function mi() {
+                window.navigator.vibrate(329,6276);
+            }
+            function fa() {
+                window.navigator.vibrate(349,2282);
+            }
+            function sol() {
+                window.navigator.vibrate(391,9954);
+            }
+            function la() {
+                window.navigator.vibrate(440,0000);
+            }
+            function si() {
+                window.navigator.vibrate(493,8833);
+            }
