fbe1e062b65ce432ed179b3450cf20bd5262d79c
saulopereira2018 <37461725+saulopereira2018@users.noreply.github.com>
2021-01-17 21:20:02 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,27 +1,85 @@
+const stopButton1 = document.querySelector('#stopButton1'),
+  stopButton2 = document.querySelector('#stopButton2'),
+  stopButton3 = document.querySelector('#stopButton3'),
+  stopButton4 = document.querySelector('#stopButton4'),
+  stopButton5 = document.querySelector('#stopButton5'),
+  stopButton6 = document.querySelector('#stopButton6'),
+  stopButton7 = document.querySelector('#stopButton7');
+  
+const c4 = 261.6,
+	d4 = 293.7,
+  e4 = 329.6,
+  f4 = 349.2,
+  g4 = 392.0,
+  a4 = 440.0,
+  b4 = 493.9;
 
-        function doo() {
-        window.navigator.vibrate(15.697,54);
-        }
-        function re() {
-        window.navigator.vibrate(17.619,89);
-        }
-        function mi() {
-        window.navigator.vibrate(19.777,66);
-        }
-        function fa() {
-        window.navigator.vibrate(20.953,69);
-        }
-        function sol() {
-        window.navigator.vibrate(23.519,72);
-        }
-        function la() {
-        window.navigator.vibrate(1.584000);
-        }
-        function si() {
-        window.navigator.vibrate(493,8833);
-        }
-        function pastorzinho() {
-        window.navigator.vibrate([doo(false), re(), mi(), fa(), fa(), fa()]);
-        }
-    
-    
+  	
+let context,
+	oscillator,
+  contextGain,
+  x = 1,
+  type = 'sine',
+  frequency;
+
+function start(){
+	context = new AudioContext();
+	oscillator = context.createOscillator();
+  contextGain = context.createGain();
+  oscillator.frequency.value = frequency;
+  oscillator.type = type;
+  oscillator.connect(contextGain);
+	contextGain.connect(context.destination);
+	oscillator.start(0);
+}
+
+function stop(){
+  start();
+  contextGain.gain.exponentialRampToValueAtTime(
+  	0.00001, context.currentTime + x
+	)
+}
+
+stopButton1.addEventListener('click', function(){
+        frequency = c4;
+        window.navigator.vibrate(1569);      
+  stop();
+
+});
+stopButton2.addEventListener('click', function(){
+        frequency = d4;
+        window.navigator.vibrate(1761);
+  stop();
+});
+stopButton3.addEventListener('click', function(){
+        frequency = e4;
+        window.navigator.vibrate(1977);
+  stop();
+});
+stopButton4.addEventListener('click', function(){
+        frequency = f4;
+        window.navigator.vibrate(2095);
+  stop();
+});
+stopButton5.addEventListener('click', function(){
+        frequency = g4;
+        window.navigator.vibrate(2351);
+  stop();
+});
+var button06 = stopButton6.addEventListener('click', function(){
+        frequency = a4;
+        window.navigator.vibrate(1584);
+      
+  stop();
+});
+stopButton7.addEventListener('click', function(){
+        frequency = b4;
+        window.navigator.vibrate(4930);
+     
+  stop();
+});
+pastorzinho.addEventListener('click', function(){
+        button06();
+     
+  stop();
+});
